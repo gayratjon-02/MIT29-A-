7d6bc44a4e6884e9bb3901b3adbfab5be02405be
gayratjon-02 <s-202386005@sillain.ac.kr>
2025-05-27 01:57:44 +0900

    externel modellarni chuqur va qoshimchalar bilan organdik

--- index.js
+++ index.js
@@ -41,6 +41,87 @@
 
 
 
+// -----------------------------------------------------------
+
+// const chalk = require('chalk');
+// // import chalk from "chalk";
+
+// console.log(chalk.red("steve"));
+
+
+
+// 
+
+// const {v4:uuidv4 } = require('uuid');
+
+// const random = uuidv4();
+
+// console.log("randomly" ,random);
+
+
+
+
+
+
+const validator = require('validator');
+
+
+// const real = validator.isEmail('abdulboriy@3gmail.com');
+
+// const real = validator.isFloat('fdd');
+
+// const real = validator.isStrongPassword('Sdzf43s!df');
+
+// console.log(real);
+
+
+
+// -------------------------------------------
+
+// const moment = require('moment');
+
+
+// // const data = moment().format("YYYY-MM-DD");
+// // const data = moment().format("hh-mm-ss");
+// const data = moment().format("MMMM Do YYYY, h:mm:ss a");
+// console.log(data);
+
+
+
+// --------------------------------------------------------
+
+// inquirer
+
+const inquirer = require('inquirer');
+// import inquirer from "inquirer";
+
+inquirer
+.prompt([{
+    type: "input",
+    name: "ism",
+    message: "ismingizni kiriting"
+}])
+.then((javob) => {
+    console.log(`sizning ismingiz: ${javob.ism}`);
+}).catch((hato) => console.log(hato) );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
