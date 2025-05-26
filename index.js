67f6361a0851addf99a6306ee3967618e6c5116b
gayratjon-02 <s-202386005@sillain.ac.kr>
2025-05-27 00:52:55 +0900

    MIT Core modules done

---
+++ index.js
@@ -0,0 +1,52 @@
+// Core Module package
+
+// setTimeout();
+
+// setTimeout(function() {
+//     console.log('ishga tushur');
+// } , 5000);
+
+
+
+// // setInterval
+// let number = 0;
+// const a = setInterval(function(){
+//     console.log('started');
+//     number++;
+
+
+//     if(number >= 5){
+//         clearInterval(a);
+//         console.log('intervalimiz tohtatildi');
+        
+//     }
+// },1000);
+
+
+// const fs = require('fs');
+
+// const data = fs.readFileSync('./mit29.txt', 'utf8');
+// console.log(data);
+
+
+// fs.writeFileSync('./mit29.txt',  ` ${data} \n\t\t this is MIT29  `);
+
+ 
+//  const new_data = fs.readFileSync('./mit29.txt', 'utf8');
+// console.log(new_data);
+
+// it is MIT29
+
+
+
+
+
+
+
+
+
+
+
+
+// External Module package
+// File Module package
