71d6e9a5f863e38b1f7e00bcf6d9e4b89b106d58
abdulboriy6863 <abdulboriy6863@gmail.com>
2025-05-28 09:44:20 -0700

    brawserga text qoshishni organdil

--- index.js
+++ index.js
@@ -1,133 +1,86 @@
-// Core Module package
+// // Core Module package
 
-// setTimeout();
+// // setTimeout();
 
-// setTimeout(function() {
-//     console.log('ishga tushur');
-// } , 5000);
+// // setTimeout(function() {
+// //     console.log('ishga tushur');
+// // } , 5000);
 
+// // // setInterval
+// // let number = 0;
+// // const a = setInterval(function(){
+// //     console.log('started');
+// //     number++;
 
+// //     if(number >= 5){
+// //         clearInterval(a);
+// //         console.log('intervalimiz tohtatildi');
 
-// // setInterval
-// let number = 0;
-// const a = setInterval(function(){
-//     console.log('started');
-//     number++;
+// //     }
+// // },1000);
 
+// // const fs = require('fs');
 
-//     if(number >= 5){
-//         clearInterval(a);
-//         console.log('intervalimiz tohtatildi');
-        
-//     }
-// },1000);
+// // const data = fs.readFileSync('./mit29.txt', 'utf8');
+// // console.log(data);
 
+// // fs.writeFileSync('./mit29.txt',  ` ${data} \n\t\t this is MIT29  `);
 
-// const fs = require('fs');
+// //  const new_data = fs.readFileSync('./mit29.txt', 'utf8');
+// // console.log(new_data);
 
-// const data = fs.readFileSync('./mit29.txt', 'utf8');
-// console.log(data);
+// // it is MIT29
 
+// // -----------------------------------------------------------
 
-// fs.writeFileSync('./mit29.txt',  ` ${data} \n\t\t this is MIT29  `);
+// // const chalk = require('chalk');
+// // // import chalk from "chalk";
 
- 
-//  const new_data = fs.readFileSync('./mit29.txt', 'utf8');
-// console.log(new_data);
+// // console.log(chalk.red("steve"));
 
-// it is MIT29
+// //
 
+// // const {v4:uuidv4 } = require('uuid');
 
+// // const random = uuidv4();
 
+// // console.log("randomly" ,random);
 
+// const validator = require('validator');
 
-// -----------------------------------------------------------
+// // const real = validator.isEmail('abdulboriy@3gmail.com');
 
-// const chalk = require('chalk');
-// // import chalk from "chalk";
+// // const real = validator.isFloat('fdd');
 
-// console.log(chalk.red("steve"));
+// // const real = validator.isStrongPassword('Sdzf43s!df');
 
+// // console.log(real);
 
+// // -------------------------------------------
 
-// 
+// // const moment = require('moment');
 
-// const {v4:uuidv4 } = require('uuid');
+// // // const data = moment().format("YYYY-MM-DD");
+// // // const data = moment().format("hh-mm-ss");
+// // const data = moment().format("MMMM Do YYYY, h:mm:ss a");
+// // console.log(data);
 
-// const random = uuidv4();
+// // --------------------------------------------------------
 
-// console.log("randomly" ,random);
+// // inquirer
 
-
-
-
-
-
-const validator = require('validator');
-
-
-// const real = validator.isEmail('abdulboriy@3gmail.com');
-
-// const real = validator.isFloat('fdd');
-
-// const real = validator.isStrongPassword('Sdzf43s!df');
-
-// console.log(real);
-
-
-
-// -------------------------------------------
-
-// const moment = require('moment');
-
-
-// // const data = moment().format("YYYY-MM-DD");
-// // const data = moment().format("hh-mm-ss");
-// const data = moment().format("MMMM Do YYYY, h:mm:ss a");
-// console.log(data);
-
-
-
-// --------------------------------------------------------
+// const inquirer = require('inquirer');
+// // import inquirer from "inquirer";
 
 // inquirer
+// .prompt([{
+//     type: "input",
+//     name: "ism",
+//     message: "ismingizni kiriting"
+// }])
+// .then((javob) => {
+//     console.log(`sizning ismingiz: ${javob.ism}`);
+// }).catch((hato) => console.log(hato) );
 
-const inquirer = require('inquirer');
-// import inquirer from "inquirer";
-
-inquirer
-.prompt([{
-    type: "input",
-    name: "ism",
-    message: "ismingizni kiriting"
-}])
-.then((javob) => {
-    console.log(`sizning ismingiz: ${javob.ism}`);
-}).catch((hato) => console.log(hato) );
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// External Module package
-// File Module package
+// // External Module package
+// // File Module package
--- validator.js
+++ validator.js
@@ -1,18 +1,27 @@
 const form = document.querySelector(".form_class");
 const input = document.getElementById("pass");
+const p = document.querySelector(".result");
+// console.log(p);
 
+form.addEventListener("submit", (bosildi) => {
+  bosildi.preventDefault();
+  const parol = input.value;
 
+  const natija = validator.isStrongPassword(parol);
+  console.log(parol, natija);
+  input.value = "";
+  input.focus();
 
-form.addEventListener('submit', (bosildi) => {
-    bosildi.preventDefault();
-    const parol = input.value;
+  if (natija) {
+    p.textContent += "this is true password";
+    p.style.color = "green";
 
-const natija = validator.isStrongPassword(parol);
-console.log(natija);
+    // console.log("natija true");
+  } else {
+    p.textContent += "this is false password";
+    p.style.color = "red";
+    p.style.fontSize = "30px";
 
-    
+    // console.log("natija false");
+  }
 });
-
-
-
-
